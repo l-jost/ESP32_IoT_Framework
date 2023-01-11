91649a24df28d13b5a423306b81b193695aebcf2
Florian Baumgartner <florian.b-h@bluewin.ch>
2023-01-11 22:08:22 +0100

    Fixed Typo in ESP32-S3

--- ESP32S2_Basic/src/console.cpp
+++ ESP32S2_Basic/src/console.cpp
@@ -188,7 +188,7 @@ void Console::printStartupMessage(void)
   stream.print(CONSOLE_CLEAR);
   stream.print(CONSOLE_COLOR_BOLD_CYAN CONSOLE_BACKGROUND_DEFAULT);
   stream.println("****************************************************");
-  stream.println("*                   ESP32-S3 Basic                 *");
+  stream.println("*                   ESP32-S2 Basic                 *");
   stream.println("*             2023, Florian Baumgartner            *");
   stream.println("****************************************************");
   stream.println(CONSOLE_LOG);
